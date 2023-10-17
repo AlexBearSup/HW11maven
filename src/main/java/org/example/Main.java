f41c5a183ff63d663977e9633237aa5637b44995
Suprun Alex <1specktr1@gmail.com>
2023-10-17 11:26:28 +0300

    add calc for 2 numbers & 4 actions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,32 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        char action = args[2].charAt(0);
+        int result;
+        switch (action){
+            case '+':
+                result = a + b;
+                System.out.println(result);
+                break;
+            case '-':
+                result = a - b;
+                System.out.println(result);
+                break;
+            case '*':
+                result = a * b;
+                System.out.println(result);
+                break;
+            case '/':
+                result = a / b;
+                System.out.println(result);
+                break;
+            default:
+                System.out.println("wrong symbol");
         }
+
     }
 }
